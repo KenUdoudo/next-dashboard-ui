c43a86c88ec2a9360a9f5393d66a75195001bbd3
Ken Udoudo <kendo302003@gmail.com>
2025-01-01 13:29:39 +0100

    width

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,8 +7,8 @@ export default function DashboardLayout({
   return (
     <html lang="en">
       <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[14%] bg-blue-600"></div>
-      <div className="w-[86%] md:w-[92%] lg:w-[86%] bg-red-400"></div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-600"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-red-400"></div>
       </div>
     </html>
   );
