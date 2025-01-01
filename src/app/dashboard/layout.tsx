41343c3de58629fcd603ed6f2d0d03c72def524b
Ken Udoudo <kendo302003@gmail.com>
2025-01-01 13:37:18 +0100

    hero

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
-
+import Link from "next/link";
+import Image from "next/image";
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -7,7 +8,12 @@ export default function DashboardLayout({
   return (
     <html lang="en">
       <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-600"></div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-600">
+        <Link href='/' className="flex items-center justify-center gap-2">
+          <Image src='/logo.png' alt='' width={32} height={32}/>
+          <span className="hidden lg:block">Schoollama</span>
+        </Link>
+      </div>
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-red-400"></div>
       </div>
     </html>
