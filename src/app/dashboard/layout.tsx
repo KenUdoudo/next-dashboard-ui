4c79645d95ca3bcc56ab866f847003430517949c
Ken Udoudo <kendo302003@gmail.com>
2025-01-01 13:26:55 +0100

    width

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <div className="h-screen flex">
+      <div className="w-[14%] md:w-[8%] lg:w-[14%] bg-blue-600"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[86%] bg-red-400"></div>
+      </div>
+    </html>
+  );
+}
